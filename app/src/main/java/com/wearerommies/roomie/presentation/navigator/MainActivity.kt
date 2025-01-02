3f3f6d6e772c413aa8540574d060af08e41a226c
hyeumm <20221159@sungshin.ac.kr>
2025-01-03 06:10:45 +0900

    setting/#2: navigator 패키지 생성 - MainActivity 추가

--- app/src/main/java/com/wearerommies/roomie/presentation/navigator/MainActivity.kt
+++ app/src/main/java/com/wearerommies/roomie/presentation/navigator/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.wearerommies.roomie
+package com.wearerommies.roomie.presentation.navigator
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -19,29 +19,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             RoomieAndroidTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RoomieAndroidTheme {
-        Greeting("Android")
     }
 }
