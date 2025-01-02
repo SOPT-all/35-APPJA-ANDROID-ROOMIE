34c7bdccd324c3168cd7cb20ce63876e9ce5093d
hyeumm <20221159@sungshin.ac.kr>
2025-01-03 06:17:21 +0900

    setting/#2: 다크모드 막기

--- app/src/main/java/com/wearerommies/roomie/RoomieApp.kt
+++ app/src/main/java/com/wearerommies/roomie/RoomieApp.kt
@@ -1,9 +1,16 @@
 package com.wearerommies.roomie
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 
 class RoomieApp : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        setDarkMode()
+    }
+
+    private fun setDarkMode(){
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     }
 }
