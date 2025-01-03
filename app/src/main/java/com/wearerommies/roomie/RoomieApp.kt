cb97ab3851f746e4a8094cead35a8ce67521ce50
hyeumm <20221159@sungshin.ac.kr>
2025-01-04 03:20:33 +0900

    setting/#2: Debug 모드일 때, Timber 설정

--- app/src/main/java/com/wearerommies/roomie/RoomieApp.kt
+++ app/src/main/java/com/wearerommies/roomie/RoomieApp.kt
@@ -2,15 +2,21 @@ package com.wearerommies.roomie
 
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
+import timber.log.Timber
 
 class RoomieApp : Application() {
     override fun onCreate() {
         super.onCreate()
 
+        setTimber()
         setDarkMode()
     }
 
-    private fun setDarkMode(){
+    private fun setTimber() {
+        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+    }
+
+    private fun setDarkMode() {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     }
 }
