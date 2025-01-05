77e3a6dfb2ff3d03d9e30d1c642adb1627267df0
hyeumm <20221159@sungshin.ac.kr>
2025-01-06 04:25:03 +0900

    chore/#2: Hilt 어노테이션 누락

--- app/src/main/java/com/wearerommies/roomie/RoomieApp.kt
+++ app/src/main/java/com/wearerommies/roomie/RoomieApp.kt
@@ -2,8 +2,10 @@ package com.wearerommies.roomie
 
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
+import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
+@HiltAndroidApp
 class RoomieApp : Application() {
     override fun onCreate() {
         super.onCreate()
