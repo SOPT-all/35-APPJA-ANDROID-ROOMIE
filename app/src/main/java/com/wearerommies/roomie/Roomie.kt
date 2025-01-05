427f2b9b8fc8f2c3b5dda3ad899284262484c6fb
hyeumm <20221159@sungshin.ac.kr>
2025-01-06 06:16:26 +0900

    Chore/#2: Application명 RommieApp -> Roomie로 수정

--- app/src/main/java/com/wearerommies/roomie/Roomie.kt
+++ app/src/main/java/com/wearerommies/roomie/Roomie.kt
@@ -6,7 +6,7 @@ import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
 @HiltAndroidApp
-class RoomieApp : Application() {
+class Roomie : Application() {
     override fun onCreate() {
         super.onCreate()
 
