c5af924fc29a6133f852fee4e48668018862498c
kangyein9892 <inwon9892@naver.com>
2025-01-09 04:06:55 +0900

    setting/#2: color 세팅

--- app/src/main/java/com/wearerommies/roomie/ui/theme/Color.kt
+++ app/src/main/java/com/wearerommies/roomie/ui/theme/Color.kt
@@ -2,10 +2,43 @@ package com.wearerommies.roomie.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Primary
+val Primary = Color(0xFF626CF6)
+val PrimaryLight1 = Color(0xFF949BFF)
+val PrimaryLight2 = Color(0xFFB0B5FF)
+val PrimaryLight3 = Color(0xFFD2D4FC)
+val PrimaryLight4 = Color(0xFFEFF0FF)
+val PrimaryLight5 = Color(0xFFFAFAFF)
+val PrimaryDark1 = Color(0xFF3C43A9)
+val PrimaryDark2 = Color(0xFF1B1F5A)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Secondary
+val Secondary = Color(0xFFFE885C)
+val SecondaryLight1 = Color(0xFFFEBD9A)
+val SecondaryLight2 = Color(0xFFFEE4CB)
+val SecondaryLight3 = Color(0xFFFFF7F0)
+
+
+// Gray Scale
+val GrayScale1 = Color(0xFFFFFFFF)
+val GrayScale2 = Color(0xFFFAFAFA)
+val GrayScale3 = Color(0xFFF4F4F4)
+val GrayScale4 = Color(0xFFEDEDED)
+val GrayScale5 = Color(0xFFD6D6D6)
+val GrayScale6 = Color(0xFFBCBCBC)
+val GrayScale7 = Color(0xFFA1A1A1)
+val GrayScale8 = Color(0xFF858585)
+val GrayScale9 = Color(0xFF696969)
+val GrayScale10 = Color(0xFF4D4D4D)
+val GrayScale11 = Color(0xFF323232)
+val GrayScale12 = Color(0xFF1A1A1A)
+
+// Transparent
+val TransparentGray1260 = Color(0x99000000)
+val TransparentGray1250 = Color(0x80000000)
+val TransparentGray140 = Color(0x4DFFFFFF)
+val TransparentGray160 = Color(0x99FFFFFF)
+val TransparentGray1210 = Color(0x1A1A1A1A)
+
+// Action
+val ActionError = Color(0xFFEA0000)
