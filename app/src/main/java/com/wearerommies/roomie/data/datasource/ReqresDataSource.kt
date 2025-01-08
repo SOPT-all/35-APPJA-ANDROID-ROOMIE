28e567219fd86ecc4fa771e18c1c74665e46bace
hyeumm <20221159@sungshin.ac.kr>
2025-01-09 03:24:04 +0900

    Delete/#2: DataSourceImpl 삭제

--- app/src/main/java/com/wearerommies/roomie/data/datasource/ReqresDataSource.kt
+++ app/src/main/java/com/wearerommies/roomie/data/datasource/ReqresDataSource.kt
@@ -1,7 +1,12 @@
 package com.wearerommies.roomie.data.datasource
 
 import com.wearerommies.roomie.data.dto.response.ResponseReqresDto
+import com.wearerommies.roomie.data.service.ReqresService
+import javax.inject.Inject
 
-interface ReqresDataSource {
-    suspend fun getReqresLists(page: Int): ResponseReqresDto
+internal class ReqresDataSource @Inject constructor(
+    private val reqresService: ReqresService
+) {
+    suspend fun getReqresLists(page: Int): ResponseReqresDto =
+        reqresService.getReqresLists(page)
 }
