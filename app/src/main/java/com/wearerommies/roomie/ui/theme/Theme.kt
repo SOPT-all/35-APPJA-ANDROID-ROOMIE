a0ad1875552d590048c2edf4999e59ec3c2a7264
kangyein9892 <inwon9892@naver.com>
2025-01-09 05:44:41 +0900

    setting/#5: theme 세팅

--- app/src/main/java/com/wearerommies/roomie/ui/theme/Theme.kt
+++ app/src/main/java/com/wearerommies/roomie/ui/theme/Theme.kt
@@ -9,6 +9,9 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
@@ -30,6 +33,26 @@ private val LightColorScheme = lightColorScheme(
     */
 )
 
+private val LocalRommieTypography = staticCompositionLocalOf<RommieTypography> {
+    error("No RommieTypography provided")
+}
+
+object RommieTheme {
+    val typography: RommieTypography
+        @Composable
+        get() = LocalRommieTypography.current
+}
+
+@Composable
+fun ProvideRommieTypography(typography: RommieTypography, content: @Composable () -> Unit) {
+    val provideTypography = remember { typography.copy() }
+    provideTypography.update(typography)
+    CompositionLocalProvider(
+        LocalRommieTypography provides provideTypography,
+        content = content
+    )
+}
+
 @Composable
 fun RoomieAndroidTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
@@ -37,19 +60,17 @@ fun RoomieAndroidTheme(
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+    val colorScheme = if (darkTheme) {
+        DarkColorScheme
+    } else {
+        LightColorScheme
     }
+    val typography = rommieTypography()
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
+    ProvideRommieTypography(typography) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            content = content
+        )
+    }
 }
