16f5ec6e10d41a35d1cf45faa52673f0a6b52c9e
hyeumm <20221159@sungshin.ac.kr>
2025-01-12 05:33:00 +0900

    Feature/#14: MapTopBar 구현

--- app/src/main/java/com/wearerommies/roomie/presentation/ui/map/component/MapTopBar.kt
+++ app/src/main/java/com/wearerommies/roomie/presentation/ui/map/component/MapTopBar.kt
@@ -1,10 +1,15 @@
 package com.wearerommies.roomie.presentation.ui.map.component
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -14,6 +19,7 @@ import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.wearerommies.roomie.R
+import com.wearerommies.roomie.presentation.core.component.SearchTextField
 import com.wearerommies.roomie.presentation.core.extension.customShadow
 import com.wearerommies.roomie.presentation.core.extension.noRippleClickable
 import com.wearerommies.roomie.ui.theme.RoomieAndroidTheme
@@ -21,10 +27,25 @@ import com.wearerommies.roomie.ui.theme.RoomieTheme
 
 @Composable
 fun MapTopBar(
-
+    onClickSearchTextField: () -> Unit,
+    onClickFilterButton: () -> Unit,
+    modifier: Modifier = Modifier
 ) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        SearchTextField(
+            isReadOnly = true,
+            modifier = Modifier
+                .weight(1f)
+                .noRippleClickable { onClickSearchTextField() }
+        )
 
-    FilterButton(onClick = {})
+        Spacer(modifier = Modifier.width(8.dp))
+
+        FilterButton(onClick = onClickFilterButton)
+    }
 }
 
 @Composable
@@ -49,6 +70,9 @@ fun FilterButton(
 @Composable
 fun MapTopBarPreview() {
     RoomieAndroidTheme {
-        MapTopBar()
+        MapTopBar(
+            onClickSearchTextField = {},
+            onClickFilterButton = {}
+        )
     }
 }
