99d70f3a6a15ac5c7312435004506fcbd66d30b2
kimsuhyeon <suhyun10201@naver.com>
2025-01-12 23:47:06 +0900

    feat/#20: roundedBackgroundWithBorder 추가

--- app/src/main/java/com/wearerommies/roomie/presentation/core/extension/Modifier.kt
+++ app/src/main/java/com/wearerommies/roomie/presentation/core/extension/Modifier.kt
@@ -1,13 +1,18 @@
 package com.wearerommies.roomie.presentation.core.extension
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 
 fun Modifier.noRippleClickable(onClick: () -> Unit): Modifier = composed {
     this.clickable(
@@ -27,4 +32,19 @@ fun Modifier.topBorder(
         end = Offset(size.width, 0f),
         strokeWidth = height,
     )
+}
+
+fun Modifier.roundedBackgroundWithBorder(
+    cornerRadius: Dp,
+    backgroundColor: Color,
+    borderColor: Color = Color.Transparent,
+    borderWidth: Dp = 0.dp,
+): Modifier {
+    return this
+        .background(backgroundColor, shape = RoundedCornerShape(cornerRadius))
+        .border(
+            width = borderWidth,
+            color = borderColor,
+            shape = RoundedCornerShape(cornerRadius)
+        )
 }
