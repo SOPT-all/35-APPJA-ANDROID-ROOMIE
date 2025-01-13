d12cc68a27a4c03e9a4a494035a4e73d1ab7c204
hyeumm <20221159@sungshin.ac.kr>
2025-01-14 07:10:06 +0900

    chore/#24: 에러 Row 추가

--- app/src/main/java/com/wearerommies/roomie/presentation/core/component/RommieTextField.kt
+++ app/src/main/java/com/wearerommies/roomie/presentation/core/component/RommieTextField.kt
@@ -7,10 +7,12 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -22,12 +24,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.wearerommies.roomie.R
 import com.wearerommies.roomie.presentation.core.extension.roundedBackgroundWithBorder
 import com.wearerommies.roomie.ui.theme.RoomieAndroidTheme
 import com.wearerommies.roomie.ui.theme.RoomieTheme
@@ -42,75 +48,98 @@ fun RommieTextField(
     textAlign: TextAlign = TextAlign.Start,
     placeHolder: String = "",
     isValidate: Boolean = true,
+    errorMessage: String? = "",
     content: @Composable () -> Unit = {}
 ) {
     var textFieldState by remember { mutableStateOf(TextFieldValue(textFieldValue)) }
     var isFocused by remember { mutableStateOf(false) }
     val focusRequester = remember { FocusRequester() }
 
-    BasicTextField(
-        value = textFieldState,
-        onValueChange = { newValue ->
-            textFieldState = newValue
-            onValueChange(newValue.text)
-        },
-        textStyle = RoomieTheme.typography.title1R16.copy(
-            color = RoomieTheme.colors.grayScale12,
-            textAlign = textAlign,
-        ),
-        singleLine = true,
-        keyboardOptions = KeyboardOptions(
-            keyboardType = keyboardType
-        ),
-        modifier = modifier
-            .fillMaxWidth()
-            .focusRequester(focusRequester)
-            .onFocusChanged { focusState ->
-                isFocused = focusState.isFocused
-            }
-            .roundedBackgroundWithBorder(
-                cornerRadius = 8.dp,
-                backgroundColor = RoomieTheme.colors.grayScale2,
-                borderColor =
-                when {
-                    isFocused -> RoomieTheme.colors.primary
-                    !isValidate -> RoomieTheme.colors.actionError
-                    else -> RoomieTheme.colors.grayScale5
-                },
-                borderWidth = 1.dp
-            )
-            .padding(paddingValues),
-        cursorBrush = SolidColor(RoomieTheme.colors.primary),
-        decorationBox = { innerTextField ->
-            Row(
-                horizontalArrangement = Arrangement.End,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Box(
-                    modifier = Modifier.weight(1f),
-                    contentAlignment = if (textAlign == TextAlign.End) Alignment.CenterEnd else Alignment.TopStart
+    Column {
+        BasicTextField(
+            value = textFieldState,
+            onValueChange = { newValue ->
+                textFieldState = newValue
+                onValueChange(newValue.text)
+            },
+            textStyle = RoomieTheme.typography.title1R16.copy(
+                color = RoomieTheme.colors.grayScale12,
+                textAlign = textAlign,
+            ),
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = keyboardType
+            ),
+            modifier = modifier
+                .fillMaxWidth()
+                .focusRequester(focusRequester)
+                .onFocusChanged { focusState ->
+                    isFocused = focusState.isFocused
+                }
+                .roundedBackgroundWithBorder(
+                    cornerRadius = 8.dp,
+                    backgroundColor = RoomieTheme.colors.grayScale2,
+                    borderColor =
+                    when {
+                        isFocused -> RoomieTheme.colors.primary
+                        !isValidate -> RoomieTheme.colors.actionError
+                        else -> RoomieTheme.colors.grayScale5
+                    },
+                    borderWidth = 1.dp
+                )
+                .padding(paddingValues),
+            cursorBrush = SolidColor(RoomieTheme.colors.primary),
+            decorationBox = { innerTextField ->
+                Row(
+                    horizontalArrangement = Arrangement.End,
+                    verticalAlignment = Alignment.CenterVertically
                 ) {
-                    innerTextField()
+                    Box(
+                        modifier = Modifier.weight(1f),
+                        contentAlignment = if (textAlign == TextAlign.End) Alignment.CenterEnd else Alignment.TopStart
+                    ) {
+                        innerTextField()
 
-                    if (textFieldState.text.isEmpty()) {
-                        Text(
-                            text = placeHolder,
-                            color = RoomieTheme.colors.grayScale7,
-                            style = RoomieTheme.typography.title1R16,
-                            textAlign = textAlign,
-                            modifier = Modifier.fillMaxWidth()
-                        )
+                        if (textFieldState.text.isEmpty()) {
+                            Text(
+                                text = placeHolder,
+                                color = RoomieTheme.colors.grayScale7,
+                                style = RoomieTheme.typography.title1R16,
+                                textAlign = textAlign,
+                                modifier = Modifier.fillMaxWidth()
+                            )
+                        }
+                    }
+
+                    if (content != {}) {
+                        Spacer(modifier = Modifier.width(2.dp))
+
+                        content()
                     }
                 }
+            }
+        )
+        if (!isValidate)
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Spacer(modifier = Modifier.height(4.dp))
 
-                if (content != {}) {
-                    Spacer(modifier = Modifier.width(2.dp))
+                Icon(
+                    imageVector = ImageVector.vectorResource(R.drawable.ic_warning_14px),
+                    contentDescription = "error",
+                    tint = Color.Unspecified
+                )
 
-                    content()
+                if (errorMessage != null) {
+                    Text(
+                        text = errorMessage,
+                        style = RoomieTheme.typography.body4R12,
+                        color = RoomieTheme.colors.actionError
+                    )
                 }
             }
-        }
-    )
+    }
 }
 
 @Preview
@@ -162,7 +191,8 @@ fun RoomieTextFieldPreview() {
                 paddingValues = PaddingValues(16.dp),
                 textFieldValue = "연남동",
                 onValueChange = {},
-                isValidate = false
+                isValidate = false,
+                errorMessage = "에러메세지 동작 테스트"
             )
         }
     }
