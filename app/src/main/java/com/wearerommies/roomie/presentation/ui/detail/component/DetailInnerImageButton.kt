8896a58d8a1633ede3fa31b235edb92c1c40ac9b
kangyein9892 <inwon9892@naver.com>
2025-01-15 03:23:32 +0900

    feat/#35:내부 이미지 둘러보기 버튼 컴포넌트 구현

--- app/src/main/java/com/wearerommies/roomie/presentation/ui/detail/component/DetailInnerImageButton.kt
+++ app/src/main/java/com/wearerommies/roomie/presentation/ui/detail/component/DetailInnerImageButton.kt
@@ -1,1 +1,33 @@
-package com.wearerommies.roomie.presentation.ui.detail.componentimport androidx.compose.foundation.layout.paddingimport androidx.compose.runtime.Composableimport androidx.compose.ui.Modifierimport androidx.compose.ui.res.stringResourceimport androidx.compose.ui.tooling.preview.Previewimport androidx.compose.ui.unit.dpimport com.wearerommies.roomie.Rimport com.wearerommies.roomie.presentation.core.component.RoomieNavigateButtonimport com.wearerommies.roomie.presentation.type.NavigateButtonTypeimport com.wearerommies.roomie.ui.theme.RoomieAndroidTheme@Composablefun DetailInnerImageButton(    modifier: Modifier = Modifier) {    RoomieNavigateButton(        type = NavigateButtonType.DETAIL,        text = stringResource(R.string.inner_image_button_text),        modifier = modifier    )}@Preview(showBackground = true)@Composablefun DetailInnerImageButtonPreview() {    RoomieAndroidTheme {        DetailInnerImageButton(            modifier = Modifier.padding(16.dp)        )    }}
+package com.wearerommies.roomie.presentation.ui.detail.component
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.wearerommies.roomie.R
+import com.wearerommies.roomie.presentation.core.component.RoomieNavigateButton
+import com.wearerommies.roomie.presentation.type.NavigateButtonType
+import com.wearerommies.roomie.ui.theme.RoomieAndroidTheme
+
+@Composable
+fun DetailInnerImageButton(
+    modifier: Modifier = Modifier
+) {
+    RoomieNavigateButton(
+        type = NavigateButtonType.DETAIL,
+        text = stringResource(R.string.inner_image_button_text),
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DetailInnerImageButtonPreview() {
+    RoomieAndroidTheme {
+        DetailInnerImageButton(
+            modifier = Modifier.padding(16.dp)
+        )
+    }
+}
