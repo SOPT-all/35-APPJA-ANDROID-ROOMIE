855ead12862769cecf23f1e6076f2b12bb3630b7
kangyein9892 <inwon9892@naver.com>
2025-01-15 06:50:30 +0900

    feature/#35: 상세 바텀바 아이콘 버튼 뷰 컴포넌트 구현

---
+++ app/src/main/java/com/wearerommies/roomie/presentation/ui/detail/component/DetailBottomIconButton.kt
@@ -0,0 +1,82 @@
+package com.wearerommies.roomie.presentation.ui.detail.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.wearerommies.roomie.R
+import com.wearerommies.roomie.presentation.core.extension.noRippleClickable
+import com.wearerommies.roomie.presentation.core.extension.roundedBackgroundWithBorder
+import com.wearerommies.roomie.ui.theme.RoomieAndroidTheme
+import com.wearerommies.roomie.ui.theme.RoomieTheme
+
+@Composable
+fun DetailBottomIconButton(
+    icon: @Composable (isPinned: Boolean) -> Unit,
+    onClickButton: () -> Unit,
+    modifier: Modifier = Modifier,
+    isPinned: Boolean = false
+) {
+    Box(
+        modifier = modifier
+                .size(56.dp)
+            .roundedBackgroundWithBorder(
+                cornerRadius = 8.dp,
+                backgroundColor = RoomieTheme.colors.grayScale1,
+                borderColor = RoomieTheme.colors.grayScale5,
+                borderWidth = 1.dp
+            )
+            .noRippleClickable(onClickButton)
+            .padding(16.dp)
+    ) {
+        icon(isPinned)
+    }
+}
+
+@Preview
+@Composable
+fun DetailBottomIconButtonPreview() {
+    RoomieAndroidTheme {
+        Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+            DetailBottomIconButton(
+                icon = { isPinned ->
+                    Icon(
+                        imageVector = if (isPinned) ImageVector.vectorResource(R.drawable.ic_heart_24px_active) else ImageVector.vectorResource(R.drawable.ic_heart_24px_active),
+                        contentDescription = null,
+                        tint = if(isPinned) RoomieTheme.colors.actionError else RoomieTheme.colors.grayScale6,
+                    )
+                },
+                onClickButton = {}
+            )
+            DetailBottomIconButton(
+                icon = { isPinned ->
+                    Icon(
+                        imageVector = if (isPinned) ImageVector.vectorResource(R.drawable.ic_heart_24px_active) else ImageVector.vectorResource(R.drawable.ic_heart_24px_active),
+                        contentDescription = null,
+                        tint = if(isPinned) RoomieTheme.colors.actionError else RoomieTheme.colors.grayScale6
+                    )
+                },
+                onClickButton = {},
+                isPinned = true
+            )
+            DetailBottomIconButton(
+                icon = {
+                    Icon(
+                        imageVector = ImageVector.vectorResource(R.drawable.ic_inquire_24px),
+                        contentDescription = null,
+                        tint = RoomieTheme.colors.grayScale6
+                    )
+                },
+                onClickButton = {}
+            )
+        }
+    }
+}
