529179bc5d65e205e62718f781dccc80214bffe1
hyeumm <20221159@sungshin.ac.kr>
2025-01-15 04:44:19 +0900

    feature/#37: EmptyView

---
+++ app/src/main/java/com/wearerommies/roomie/presentation/ui/search/component/EmptyView.kt
@@ -0,0 +1,66 @@
+package com.wearerommies.roomie.presentation.ui.search.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.wearerommies.roomie.R
+import com.wearerommies.roomie.presentation.type.EmptyViewType
+import com.wearerommies.roomie.ui.theme.RoomieAndroidTheme
+import com.wearerommies.roomie.ui.theme.RoomieTheme
+
+@Composable
+fun EmptyView(
+    viewType: EmptyViewType,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            imageVector = ImageVector.vectorResource(R.drawable.ic_launcher_background),
+            contentDescription = null,
+            modifier = Modifier
+                .size((LocalConfiguration.current.screenWidthDp * 0.444).dp)
+                .clip(CircleShape)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = stringResource(viewType.title),
+            color = RoomieTheme.colors.grayScale12,
+            style = RoomieTheme.typography.title2Sb16,
+        )
+
+        Spacer(modifier = Modifier.height(4.dp))
+
+        Text(
+            text = stringResource(viewType.description),
+            color = RoomieTheme.colors.grayScale7,
+            style = RoomieTheme.typography.body4R12,
+        )
+    }
+}
+
+@Preview
+@Composable
+fun EmptyViewPreview() {
+    RoomieAndroidTheme {
+        EmptyView(viewType = EmptyViewType.LIST)
+    }
+}
