0309e75fa513c9987d5b84770f6d0d0cdde11bd2
hyeumm <20221159@sungshin.ac.kr>
2025-01-14 17:07:34 +0900

    add/#19: DateFormatter 추가

---
+++ app/src/main/java/com/wearerommies/roomie/presentation/core/util/DateFormatter.kt
@@ -0,0 +1,15 @@
+package com.wearerommies.roomie.presentation.core.util
+
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+fun Date.toFormattedString(pattern: String = "yyyy/MM/dd"): String {
+    val dateFormat = SimpleDateFormat(pattern, Locale.getDefault())
+    return dateFormat.format(this)
+}
+
+fun todayFormatted(pattern: String = "yyyy/MM/dd"): String {
+    return Date().toFormattedString(pattern)
+}
+
