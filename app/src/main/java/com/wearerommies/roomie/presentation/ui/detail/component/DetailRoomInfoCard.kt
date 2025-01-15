7bcf5a2708ca69bc2ce96713132786686c5e1f30
kangyein9892 <inwon9892@naver.com>
2025-01-15 17:13:03 +0900

    feature/#35: 룸 상세 정보 카드뷰 컴포넌트 구현

---
+++ app/src/main/java/com/wearerommies/roomie/presentation/ui/detail/component/DetailRoomInfoCard.kt
@@ -0,0 +1,127 @@
+package com.wearerommies.roomie.presentation.ui.detail.component
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.wearerommies.roomie.R
+import com.wearerommies.roomie.presentation.core.extension.noRippleClickable
+import com.wearerommies.roomie.presentation.core.extension.roundedBackgroundWithBorder
+import com.wearerommies.roomie.presentation.core.util.RegexConstants
+import com.wearerommies.roomie.ui.theme.RoomieAndroidTheme
+import com.wearerommies.roomie.ui.theme.RoomieTheme
+
+@Composable
+fun DetailRoomInfoCard(
+    roomStatus: Boolean,
+    roomName: String,
+    occupancyType: Int,
+    gender: String,
+    deposit: String,
+    prepaidUtilities: String,
+    monthlyRent: String,
+    contractPeriod: String,
+    managementFee: String,
+    onClickDetailRoomInfoCard: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .roundedBackgroundWithBorder(
+                cornerRadius = 8.dp,
+                backgroundColor = RoomieTheme.colors.grayScale1,
+                borderColor = RoomieTheme.colors.grayScale5,
+                borderWidth = 1.dp
+            )
+            .padding(16.dp)
+            .noRippleClickable(onClickDetailRoomInfoCard)
+
+    ) {
+
+        Row{
+            DetailRoomInfoStatusChip(
+                status = roomStatus
+            )
+
+            Spacer(Modifier.weight(1f))
+
+
+            Icon(
+                imageVector = ImageVector.vectorResource(R.drawable.ic_arrow_right_line_black_24px),
+                contentDescription = null,
+                tint = RoomieTheme.colors.grayScale10
+            )
+
+        }
+
+        Spacer(Modifier.height(8.dp))
+
+        Text(
+            text = roomName,
+            style = RoomieTheme.typography.heading5Sb18,
+            color = RoomieTheme.colors.grayScale11
+        )
+
+        Spacer(Modifier.height(16.dp))
+
+        DetailRoomInfoTexts(
+            occupancyType = occupancyType,
+            gender = gender,
+            deposit = deposit,
+            prepaidUtilities = prepaidUtilities,
+            monthlyRent = monthlyRent,
+            contractPeriod = contractPeriod,
+            managementFee = managementFee
+        )
+
+    }
+}
+
+@Preview
+@Composable
+fun DetailRoomInfoCardPreview() {
+    RoomieAndroidTheme {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            DetailRoomInfoCard(
+                roomStatus = true,
+                roomName = "룸 A",
+                onClickDetailRoomInfoCard = {},
+                occupancyType = 2,
+                gender = "여성",
+                deposit = "5,000,000원",
+                prepaidUtilities = "10,000원",
+                monthlyRent = "500,000원",
+                contractPeriod = "24-12-20",
+                managementFee = "100,000원"
+            )
+
+            DetailRoomInfoCard(
+                roomStatus = false,
+                roomName = "룸 A",
+                onClickDetailRoomInfoCard = {},
+                occupancyType = 2,
+                gender = "여성",
+                deposit = "5,000,000원",
+                prepaidUtilities = "10,000원",
+                monthlyRent = "500,000원",
+                contractPeriod = "24-12-20",
+                managementFee = "100,000원"
+            )
+        }
+    }
+}
