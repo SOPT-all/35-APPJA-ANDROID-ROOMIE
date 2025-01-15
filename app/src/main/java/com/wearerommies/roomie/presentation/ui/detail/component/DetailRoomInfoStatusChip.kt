ff47e0959e7a7cd1cc8c0d96e608d9a92c176b26
kangyein9892 <inwon9892@naver.com>
2025-01-15 23:12:52 +0900

    feature/#35: DetailRoomInfoStatusChipPreview 추가

--- app/src/main/java/com/wearerommies/roomie/presentation/ui/detail/component/DetailRoomInfoStatusChip.kt
+++ app/src/main/java/com/wearerommies/roomie/presentation/ui/detail/component/DetailRoomInfoStatusChip.kt
@@ -1,10 +1,15 @@
 package com.wearerommies.roomie.presentation.ui.detail.component
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.wearerommies.roomie.R
 import com.wearerommies.roomie.presentation.core.component.RoomieTextChip
+import com.wearerommies.roomie.ui.theme.RoomieAndroidTheme
 import com.wearerommies.roomie.ui.theme.RoomieTheme
 
 @Composable
@@ -19,4 +24,22 @@ fun DetailRoomInfoStatusChip(
         textColor = if (status) RoomieTheme.colors.primary else RoomieTheme.colors.grayScale9,
         backgroundColor = if (status) RoomieTheme.colors.primaryLight4 else RoomieTheme.colors.grayScale3
     )
+}
+
+@Preview
+@Composable
+fun DetailRoomInfoStatusChipPreview() {
+    RoomieAndroidTheme {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            DetailRoomInfoStatusChip(
+                status = true
+            )
+
+            DetailRoomInfoStatusChip(
+                status = false
+            )
+        }
+    }
 }
