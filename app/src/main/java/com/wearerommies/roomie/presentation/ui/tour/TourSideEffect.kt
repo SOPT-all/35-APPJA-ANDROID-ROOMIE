952529f2195564d33b2840e97ff741f3f32e0fe3
kangyein9892 <inwon9892@naver.com>
2025-01-19 23:31:38 +0900

    chore/#53: navigateToTwoStep으로 수정

--- app/src/main/java/com/wearerommies/roomie/presentation/ui/tour/TourSideEffect.kt
+++ app/src/main/java/com/wearerommies/roomie/presentation/ui/tour/TourSideEffect.kt
@@ -1,6 +1,6 @@
 package com.wearerommies.roomie.presentation.ui.tour
 
 sealed class TourSideEffect {
-    data class navigateTwoStep(val rooms: List<Int>): TourSideEffect()
+    data class navigateToTwoStep(val rooms: List<Int>): TourSideEffect()
     // data class navigationThreeStep()
 }
