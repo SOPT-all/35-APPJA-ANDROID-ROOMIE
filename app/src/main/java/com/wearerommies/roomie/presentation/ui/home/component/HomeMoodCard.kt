d014e73c302a917ae9f2ceeb200b5b12fc865c61
kimsuhyeon <suhyun10201@naver.com>
2025-01-19 22:54:39 +0900

    chore/#55: ripple 효과 모서리 clip으로 조절

--- app/src/main/java/com/wearerommies/roomie/presentation/ui/home/component/HomeMoodCard.kt
+++ app/src/main/java/com/wearerommies/roomie/presentation/ui/home/component/HomeMoodCard.kt
@@ -9,11 +9,13 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -40,6 +42,9 @@ fun HomeMoodCard(
                 backgroundColor = RoomieTheme.colors.primaryLight5,
                 borderColor = RoomieTheme.colors.primaryLight3,
                 borderWidth = 1.dp
+            )
+            .clip(
+                shape = RoundedCornerShape(8.dp)
             )
             .clickable {
                 onClick()
