63c15e219466ef1406726fbd14555fe03d56645e
kangyein9892 <inwon9892@naver.com>
2025-01-19 23:25:39 +0900

    chore/#53: 공백 삭제 및 타입 수정

--- app/src/main/java/com/wearerommies/roomie/domain/entity/TourEntity.kt
+++ app/src/main/java/com/wearerommies/roomie/domain/entity/TourEntity.kt
@@ -3,12 +3,12 @@ package com.wearerommies.roomie.domain.entity
 import java.util.Date
 
 data class TourEntity(
-    val name: String ,
+    val name: String,
     val birth: Date,
     val gender: String,
     val phoneNumber: String,
     val preferredDate: Date,
     val message: String,
-    val roomId: Int,
-    val houseId: Int
+    val roomId: Long,
+    val houseId: Long
 )
