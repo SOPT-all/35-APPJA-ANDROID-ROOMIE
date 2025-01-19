711eecda78039c3f4f0570f5b39ae4b31bb11acf
hyeumm <20221159@sungshin.ac.kr>
2025-01-20 02:43:44 +0900

    chore/#43: FilterChip 클릭 효과 추가

--- app/src/main/java/com/wearerommies/roomie/presentation/ui/filter/component/FilterChip.kt
+++ app/src/main/java/com/wearerommies/roomie/presentation/ui/filter/component/FilterChip.kt
@@ -4,8 +4,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.wearerommies.roomie.R
+import com.wearerommies.roomie.presentation.core.extension.noRippleClickable
 import com.wearerommies.roomie.presentation.core.extension.roundedBackgroundWithBorder
 import com.wearerommies.roomie.ui.theme.RoomieAndroidTheme
 import com.wearerommies.roomie.ui.theme.RoomieTheme
@@ -13,20 +17,24 @@ import com.wearerommies.roomie.ui.theme.RoomieTheme
 @Composable
 fun FilterChip(
     text: String,
-    modifier: Modifier = Modifier
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    isSelected: Boolean = false,
 ) {
     Text(
         text = text,
         style = RoomieTheme.typography.body1R14,
-        color = RoomieTheme.colors.grayScale11,
+        color = if (isSelected) RoomieTheme.colors.primary else RoomieTheme.colors.grayScale11,
+        textAlign = TextAlign.Center,
         modifier = modifier
             .roundedBackgroundWithBorder(
                 cornerRadius = 4.dp,
-                backgroundColor = RoomieTheme.colors.grayScale1,
-                borderColor = RoomieTheme.colors.grayScale5,
+                backgroundColor = if (isSelected) RoomieTheme.colors.primaryLight5 else RoomieTheme.colors.grayScale1,
+                borderColor = if (isSelected) RoomieTheme.colors.primary else RoomieTheme.colors.grayScale5,
                 borderWidth = 1.dp
             )
             .padding(vertical = 6.dp, horizontal = 12.dp)
+            .noRippleClickable { onClick() }
     )
 }
 
@@ -35,7 +43,20 @@ fun FilterChip(
 fun FilterChipPreview() {
     RoomieAndroidTheme {
         FilterChip(
-            text = "1인실"
+            text = stringResource(R.string.deposit),
+            onClick = {}
+        )
+    }
+}
+
+@Preview
+@Composable
+fun FilterChipSelectedPreview() {
+    RoomieAndroidTheme {
+        FilterChip(
+            text = stringResource(R.string.apply),
+            isSelected = false,
+            onClick = {}
         )
     }
 }
