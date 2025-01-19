2c589c1ca334d622edbb08f01dca8d358e1199be
kangyein9892 <inwon9892@naver.com>
2025-01-19 18:12:36 +0900

    delete/#49: 필요없는 주석 삭제

--- app/src/main/java/com/wearerommies/roomie/presentation/ui/tour/TourState.kt
+++ app/src/main/java/com/wearerommies/roomie/presentation/ui/tour/TourState.kt
@@ -5,7 +5,6 @@ import com.wearerommies.roomie.presentation.core.util.UiState
 
 data class TourState(
     //val uiState: TourEntity = TourEntity()
-    // one step state로 넣어야하나?
     val houseName: String,
     val roomName: List<String>
 )
