1b9353e9cba8e67a3a83288737d014c63afaa436
hyeumm <20221159@sungshin.ac.kr>
2025-01-21 00:21:55 +0900

    feature/#69: Splash 적용

--- app/src/main/java/com/wearerommies/roomie/presentation/navigator/MainActivity.kt
+++ app/src/main/java/com/wearerommies/roomie/presentation/navigator/MainActivity.kt
@@ -4,20 +4,47 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.wearerommies.roomie.presentation.ui.splash.SplashScreen
 import com.wearerommies.roomie.ui.theme.RoomieAndroidTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        installSplashScreen().setOnExitAnimationListener { splashScreenView ->
+            splashScreenView.remove()
+        }
+
         setContent {
             val navigator: MainNavigator = rememberMainNavigator()
+            var showSplash by remember { mutableStateOf(true) }
 
             RoomieAndroidTheme {
-                MainScreen(navigator = navigator)
+                LaunchedEffect(Unit) {
+                    delay(SPLASH_SCREEN_DELAY)
+                    showSplash = false
+                }
+                if (showSplash) {
+                    SplashScreen()
+                } else {
+                    MainScreen(
+                        navigator = navigator
+                    )
+                }
             }
         }
+    }
+    companion object {
+        const val SPLASH_SCREEN_DELAY = 2000L
     }
 }
