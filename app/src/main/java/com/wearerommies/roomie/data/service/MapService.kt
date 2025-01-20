1b260f2539b144e8d137a90409d3a5564e05f23f
hyeumm <20221159@sungshin.ac.kr>
2025-01-21 05:53:33 +0900

    feature/#73: MapService

---
+++ app/src/main/java/com/wearerommies/roomie/data/service/MapService.kt
@@ -0,0 +1,13 @@
+package com.wearerommies.roomie.data.service
+
+import com.wearerommies.roomie.data.dto.response.BaseResponse
+import com.wearerommies.roomie.data.dto.response.ResponseSearchDto
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface MapService {
+    @GET("/v1/locations")
+    suspend fun getSearchResult(
+        @Query("q") q: String
+    ): BaseResponse<ResponseSearchDto>
+}
