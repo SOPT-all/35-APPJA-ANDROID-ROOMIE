f110223566877347979df066eb2ba3fd77ce5aaf
hyeumm <20221159@sungshin.ac.kr>
2025-01-21 05:54:59 +0900

    chore/#73: type float로 변경

--- app/src/main/java/com/wearerommies/roomie/domain/entity/SearchResultEntity.kt
+++ app/src/main/java/com/wearerommies/roomie/domain/entity/SearchResultEntity.kt
@@ -1,8 +1,8 @@
 package com.wearerommies.roomie.domain.entity
 
 data class SearchResultEntity(
-    val x: Double,
-    val y: Double,
+    val x: Float,
+    val y: Float,
     val location: String,
     val address: String,
     val roadAddress: String
