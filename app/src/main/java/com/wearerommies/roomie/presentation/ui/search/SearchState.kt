d6f3a322a6dcd95494d25cbddfd01e394055d2a5
hyeumm <20221159@sungshin.ac.kr>
2025-01-21 05:56:20 +0900

    chore/#73: uiState 기본값을 success로 변경

--- app/src/main/java/com/wearerommies/roomie/presentation/ui/search/SearchState.kt
+++ app/src/main/java/com/wearerommies/roomie/presentation/ui/search/SearchState.kt
@@ -3,8 +3,11 @@ package com.wearerommies.roomie.presentation.ui.search
 import com.wearerommies.roomie.domain.entity.SearchResultEntity
 import com.wearerommies.roomie.presentation.core.util.EmptyUiState
 import kotlinx.collections.immutable.PersistentList
+import kotlinx.collections.immutable.persistentListOf
 
 data class SearchState(
-    val uiState: EmptyUiState<PersistentList<SearchResultEntity>> = EmptyUiState.Loading,
+    val uiState: EmptyUiState<PersistentList<SearchResultEntity>> = EmptyUiState.Success(
+        persistentListOf()
+    ),
     val searchKeyword: String = ""
 )
