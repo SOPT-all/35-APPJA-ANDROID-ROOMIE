65791708938020be531e0b97ada2a5627b3cb5a5
kangyein9892 <inwon9892@naver.com>
2025-01-20 15:43:59 +0900

    feature/#57: DetailHouse 임시 sideeffect, state, viewmodel 설계

---
+++ app/src/main/java/com/wearerommies/roomie/presentation/ui/detail/Image/DetailHouseSideEffect.kt
@@ -0,0 +1,5 @@
+package com.wearerommies.roomie.presentation.ui.detail.Image
+
+sealed class DetailHouseSideEffect {
+    data object navigateUp: DetailHouseSideEffect()
+}
---
+++ app/src/main/java/com/wearerommies/roomie/presentation/ui/detail/Image/DetailHouseViewModel.kt
@@ -0,0 +1,27 @@
+package com.wearerommies.roomie.presentation.ui.detail.Image
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import javax.inject.Inject
+
+@HiltViewModel
+class DetailHouseViewModel @Inject constructor(
+
+): ViewModel() {
+
+    private val _state = MutableStateFlow(DetailHouseState())
+    val state: StateFlow<DetailHouseState>
+        get() = _state.asStateFlow()
+
+    private val _sideEffect: MutableSharedFlow<DetailHouseSideEffect> = MutableSharedFlow()
+    val sideEffect: SharedFlow<DetailHouseSideEffect>
+        get() = _sideEffect.asSharedFlow()
+
+
+}
