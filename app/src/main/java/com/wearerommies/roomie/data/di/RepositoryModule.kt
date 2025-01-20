cc3dc3f95425d50c6045969b590a8910a96c74f2
hyeumm <20221159@sungshin.ac.kr>
2025-01-21 05:54:43 +0900

    feature/#73: bindsMapRepository

--- app/src/main/java/com/wearerommies/roomie/data/di/RepositoryModule.kt
+++ app/src/main/java/com/wearerommies/roomie/data/di/RepositoryModule.kt
@@ -1,11 +1,13 @@
 package com.wearerommies.roomie.data.di
 
-import com.wearerommies.roomie.data.repositoryimpl.UserRepositoryImpl
 import com.wearerommies.roomie.data.repositoryimpl.HouseRepositoryImpl
+import com.wearerommies.roomie.data.repositoryimpl.MapRepositoryImpl
 import com.wearerommies.roomie.data.repositoryimpl.ReqresRepositoryImpl
-import com.wearerommies.roomie.domain.repository.UserRepository
+import com.wearerommies.roomie.data.repositoryimpl.UserRepositoryImpl
 import com.wearerommies.roomie.domain.repository.HouseRepository
+import com.wearerommies.roomie.domain.repository.MapRepository
 import com.wearerommies.roomie.domain.repository.ReqresRepository
+import com.wearerommies.roomie.domain.repository.UserRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -26,4 +28,8 @@ internal interface RepositoryModule {
     @Binds
     @Singleton
     fun bindsHouseRepository(houseRepositoryImpl: HouseRepositoryImpl): HouseRepository
+
+    @Binds
+    @Singleton
+    fun bindsMapRepository(mapRepositoryImpl: MapRepositoryImpl): MapRepository
 }
