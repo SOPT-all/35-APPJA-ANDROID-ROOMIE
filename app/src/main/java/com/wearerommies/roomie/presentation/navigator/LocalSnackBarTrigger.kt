d1b092940b3631f4b4d927a6f421296f2fef52db
kimsuhyeon <suhyun10201@naver.com>
2025-01-22 06:01:13 +0900

    del/#63: 필요 없는 코드 삭제

--- app/src/main/java/com/wearerommies/roomie/presentation/navigator/LocalSnackBarTrigger.kt
+++
@@ -1,7 +0,0 @@
-package com.wearerommies.roomie.presentation.navigator
-
-import androidx.compose.runtime.staticCompositionLocalOf
-
-val LocalSnackBarTrigger = staticCompositionLocalOf<String> {
-    error("No SnackBar provided")
-}
