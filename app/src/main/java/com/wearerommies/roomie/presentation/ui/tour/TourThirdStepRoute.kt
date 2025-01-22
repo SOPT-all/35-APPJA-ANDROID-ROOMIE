23d9679f60d7762c879bf53b0c8e4c1e043eb511
kangyein9892 <inwon9892@naver.com>
2025-01-23 05:46:36 +0900

    feature/#93: 세번째 화면 상태 관리

--- app/src/main/java/com/wearerommies/roomie/presentation/ui/tour/TourThirdStepRoute.kt
+++ app/src/main/java/com/wearerommies/roomie/presentation/ui/tour/TourThirdStepRoute.kt
@@ -10,6 +10,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalConfiguration
@@ -18,9 +20,12 @@ import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.wearerommies.roomie.R
 import com.wearerommies.roomie.presentation.core.component.RommieTextField
 import com.wearerommies.roomie.presentation.core.component.RoomieButton
+import com.wearerommies.roomie.presentation.core.component.RoomieDatePicker
 import com.wearerommies.roomie.presentation.core.component.RoomieDatePickerFieldWithTitle
 import com.wearerommies.roomie.presentation.core.component.RoomieTopBar
 import com.wearerommies.roomie.presentation.core.extension.noRippleClickable
@@ -32,9 +37,31 @@ import com.wearerommies.roomie.ui.theme.RoomieTheme
 fun TourThirdStepRoute(
     paddingValues: PaddingValues,
     navigateUp: () -> Unit,
+    navigateCompletedStep: () -> Unit,
     viewModel: TourViewModel = hiltViewModel()
 ) {
+    val state by viewModel.state.collectAsStateWithLifecycle()
+    val lifecycleOwner = LocalLifecycleOwner.current
 
+    LaunchedEffect(viewModel.sideEffect, lifecycleOwner) {
+        viewModel.sideEffect.collect { sideEffect ->
+            when (sideEffect) {
+                TourSideEffect.NavigateUp -> navigateUp()
+                TourSideEffect.NavigateToCompletedStep -> navigateCompletedStep()
+                else -> return@collect
+            }
+        }
+    }
+
+    TourThirdStepScreen(
+        paddingValues = paddingValues,
+        navigateUp = viewModel::navigateUp,
+        navigateCompletedStep = viewModel::navigateCompletedStep,
+        onPreferredDateChanged = viewModel::updatePreferredDate,
+        onMessageChanged = viewModel::updateMessage,
+        updatePreferredDateModalState = viewModel::updatePreferredDateModalState,
+        state = state
+    )
 }
 
 @Composable
@@ -42,11 +69,25 @@ fun TourThirdStepScreen(
     paddingValues: PaddingValues,
     navigateUp: () -> Unit,
     navigateCompletedStep: () -> Unit,
-    onPreferredDateChanged: (String) -> Unit,
-    onClickGenderButton: (String) -> Unit,
+    onPreferredDateChanged: (Long?) -> Unit,
+    onMessageChanged: (String?) -> Unit,
+    updatePreferredDateModalState: () -> Unit,
     state: TourState,
     modifier: Modifier = Modifier
 ) {
+
+    if (state.isShowPreferredDateModal)
+        RoomieDatePicker(
+            onConfirm = { date ->
+                onPreferredDateChanged(date)
+                updatePreferredDateModalState()
+            },
+            onDismiss = {
+                updatePreferredDateModalState()
+            },
+            modifier = Modifier.padding(horizontal = 36.dp)
+        )
+
     Column(
         modifier = modifier
             .fillMaxWidth()
@@ -81,10 +122,9 @@ fun TourThirdStepScreen(
 
         RoomieDatePickerFieldWithTitle(
             viewType = DatePickerFieldType.TOUR,
-            dateValue = "", // state.perrferedDate
+            dateValue = state.uiState.preferredDate,
             onClick = {
-                // ToDo: date picker 열기
-                onPreferredDateChanged("")
+                updatePreferredDateModalState()
             },
             modifier = Modifier
                 .padding(horizontal = 16.dp)
@@ -107,8 +147,8 @@ fun TourThirdStepScreen(
         RommieTextField(
             paddingValues = PaddingValues(16.dp),
             placeHolder = stringResource(R.string.tour_message_placeholder),
-            onValueChange = {},
-            textFieldValue = "",
+            onValueChange = onMessageChanged,
+            textFieldValue = state.uiState.message ?: "",
             modifier = Modifier
                 .padding(horizontal = 16.dp)
                 .height((LocalConfiguration.current.screenHeightDp * 0.143).dp),
@@ -121,11 +161,11 @@ fun TourThirdStepScreen(
             text = stringResource(R.string.tour_apply_write_complete),
             backgroundColor = RoomieTheme.colors.primary,
             textColor = RoomieTheme.colors.grayScale1,
-            onClick = {},
+            onClick = navigateCompletedStep,
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(20.dp),
-            isEnabled = true, // TODO: 버튼 활성화 상태 관리
+            isEnabled = state.isThirdEnabledButton,
             isPressed = true,
             pressedColor = RoomieTheme.colors.primaryLight1,
             enabledColor = RoomieTheme.colors.grayScale6
@@ -142,11 +182,12 @@ fun TourThirdStepScreenPreview() {
             navigateUp = {},
             navigateCompletedStep = {},
             onPreferredDateChanged = {},
-            onClickGenderButton = {},
             state = TourState(
                 houseName = "해피쉐어 루미 건대점",
                 roomName = "1A(싱글배드)"
-            )
+            ),
+            onMessageChanged = {},
+            updatePreferredDateModalState = {}
         )
     }
 }
