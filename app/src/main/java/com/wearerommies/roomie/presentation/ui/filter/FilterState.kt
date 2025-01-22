87c13d3e6975cd1d1e92ef900d3a33f8db4ab312
hyeumm <20221159@sungshin.ac.kr>
2025-01-23 00:37:41 +0900

    chore/#89: FilterState 수정

--- app/src/main/java/com/wearerommies/roomie/presentation/ui/filter/FilterState.kt
+++ app/src/main/java/com/wearerommies/roomie/presentation/ui/filter/FilterState.kt
@@ -5,8 +5,8 @@ import kotlinx.collections.immutable.persistentListOf
 
 data class FilterState(
     val isDateModalOpened: Boolean = false,
-    val location: String = "",
-    val moodTag: String = "",
+    val location: String = "서울특별시 마포구 노고산동 11-1", // TODO: state 위치 고정??
+    val moodTag: String? = null,
     val depositStart: Int = 0,
     val depositEnd: Int = 500,
     val monthlyRentStart: Int = 0,
