8fac52b0a68da40226499ae0cac1592614756eec
kangyein9892 <inwon9892@naver.com>
2025-01-23 02:29:04 +0900

    feature/#93: TourState 구현

--- app/src/main/java/com/wearerommies/roomie/presentation/ui/tour/TourState.kt
+++ app/src/main/java/com/wearerommies/roomie/presentation/ui/tour/TourState.kt
@@ -4,7 +4,7 @@ import com.wearerommies.roomie.domain.entity.TourEntity
 import com.wearerommies.roomie.presentation.core.util.UiState
 
 data class TourState(
-    //val uiState: TourEntity = TourEntity()
-    val houseName: String,
-    val roomName: String
+    val uiState: TourEntity = TourEntity(),
+    val houseName: String = "",
+    val roomName: String = ""
 )
