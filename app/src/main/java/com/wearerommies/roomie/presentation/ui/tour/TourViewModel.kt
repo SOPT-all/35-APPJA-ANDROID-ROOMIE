f34bd208bb0abd5caf149d3480a2fda8221bf51e
kangyein9892 <inwon9892@naver.com>
2025-01-23 02:29:44 +0900

    feature/#93: tourviewmodel 초기 상태 구현

--- app/src/main/java/com/wearerommies/roomie/presentation/ui/tour/TourViewModel.kt
+++ app/src/main/java/com/wearerommies/roomie/presentation/ui/tour/TourViewModel.kt
@@ -1,10 +1,16 @@
 package com.wearerommies.roomie.presentation.ui.tour
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.wearerommies.roomie.domain.entity.TourEntity
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -12,13 +18,27 @@ class TourViewModel @Inject constructor(
 
 ): ViewModel() {
 
-//    private val _state = MutableStateFlow(TourState())
-//    val state: StateFlow<TourState>
-//        get() = _state.asStateFlow()
+    private val _state = MutableStateFlow(TourState())
+    val state: StateFlow<TourState>
+        get() = _state.asStateFlow()
 
     private val _sideEffect: MutableSharedFlow<TourSideEffect> = MutableSharedFlow()
     val sideEffect: SharedFlow<TourSideEffect>
         get() = _sideEffect.asSharedFlow()
 
+    fun navigateSecondStep()  = viewModelScope.launch {
+        _sideEffect.emit(TourSideEffect.NavigateToTwoStep)
+    }
+
+    suspend fun initState(houseId: Long, roomId: Long, houseName: String, roomName: String) {
+        _state.value = _state.value.copy(
+            TourEntity(
+                houseId = houseId,
+                roomId = roomId
+            ),
+            houseName = houseName,
+            roomName = roomName
+        )
+    }
 
 }
