454b8c88bff40cf0d947d9d5445d26d3d4322c0f
kangyein9892 <inwon9892@naver.com>
2025-01-23 04:44:19 +0900

    feature/#93: 뒤로가기, 투어 두번째 화면 이동 sideeffect 구현

--- app/src/main/java/com/wearerommies/roomie/presentation/ui/tour/TourSideEffect.kt
+++ app/src/main/java/com/wearerommies/roomie/presentation/ui/tour/TourSideEffect.kt
@@ -2,4 +2,6 @@ package com.wearerommies.roomie.presentation.ui.tour
 
 sealed class TourSideEffect {
     data object NavigateToTwoStep: TourSideEffect()
+    data object NavigateUp: TourSideEffect()
+    data object NavigateToSecondStep: TourSideEffect()
 }
