4e227cd07f3583034121c8a531726f3e1afced0b
hyeumm <20221159@sungshin.ac.kr>
2025-01-22 20:18:52 +0900

    chore/#89: Serializable 어노테이션 추가

--- app/src/main/java/com/wearerommies/roomie/domain/entity/FilterEntity.kt
+++ app/src/main/java/com/wearerommies/roomie/domain/entity/FilterEntity.kt
@@ -1,22 +1,27 @@
 package com.wearerommies.roomie.domain.entity
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class FilterEntity(
-    val location: String,
-    val moodTag: String?,
-    val depositRange: DepositRange,
-    val monthlyRentRange: MonthlyRentRange,
-    val genderPolicy: List<String>,
-    val preferredDate: String?,
-    val occupancyTypes: List<String>,
-    val contractPeriod: List<Int>
-){
+    val location: String = "",
+    val moodTag: String? = "",
+    val depositRange: DepositRange = DepositRange(),
+    val monthlyRentRange: MonthlyRentRange = MonthlyRentRange(),
+    val genderPolicy: List<String> = listOf(),
+    val preferredDate: String? = "",
+    val occupancyTypes: List<String> = listOf(),
+    val contractPeriod: List<Int> = listOf()
+) {
+    @Serializable
     data class DepositRange(
-        val min: Int,
-        val max: Int
+        val min: Int = 0,
+        val max: Int = 500
     )
 
+    @Serializable
     data class MonthlyRentRange(
-        val min: Int,
-        val max: Int
+        val min: Int = 0,
+        val max: Int = 150
     )
 }
