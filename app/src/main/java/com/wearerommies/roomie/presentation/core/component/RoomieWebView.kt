2d66ed29eea1cb4f26e0185e4f66f6d54a879185
kimsuhyeon <suhyun10201@naver.com>
2025-01-24 06:26:36 +0900

    feat/#102: webview status bar 색상 조정

--- app/src/main/java/com/wearerommies/roomie/presentation/core/component/RoomieWebView.kt
+++ app/src/main/java/com/wearerommies/roomie/presentation/core/component/RoomieWebView.kt
@@ -2,6 +2,7 @@ package com.wearerommies.roomie.presentation.core.component
 
 import android.webkit.WebViewClient
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.statusBarsPadding
@@ -13,6 +14,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import com.google.accompanist.web.WebView
 import com.google.accompanist.web.rememberWebViewState
+import com.wearerommies.roomie.ui.theme.RoomieTheme
 
 @Composable
 fun RoomieWebView(url: String, onClose: () -> Unit) {
@@ -31,6 +33,7 @@ fun RoomieWebView(url: String, onClose: () -> Unit) {
         WebView(
             state = webViewState,
             modifier = Modifier
+                .background(color = RoomieTheme.colors.grayScale1)
                 .statusBarsPadding()
                 .weight(1f),
             onCreated = { webViewInstance ->
