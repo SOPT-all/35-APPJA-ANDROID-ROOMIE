ee08f697bf910b699fdde99e088ae6039bfe8e76
kangyein9892 <inwon9892@naver.com>
2025-01-23 22:43:06 +0900

    chore/#93: 서버에 보낼 때 사용할 dateformatter 추가

--- app/src/main/java/com/wearerommies/roomie/presentation/core/util/DateFormatter.kt
+++ app/src/main/java/com/wearerommies/roomie/presentation/core/util/DateFormatter.kt
@@ -13,3 +13,8 @@ fun todayFormatted(pattern: String = "yyyy/MM/dd"): String {
     return Date().toFormattedString(pattern)
 }
 
+fun String.toFormattedDto(): String {
+    val parts = this.split("/")
+    return "${parts[0]}-${parts[1]}-${parts[2]}"
+}
+
