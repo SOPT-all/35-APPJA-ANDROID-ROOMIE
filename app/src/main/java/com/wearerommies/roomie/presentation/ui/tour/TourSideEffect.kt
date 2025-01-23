7f655d8ca099fd7cc74e83f6a1d34733c3d9b0bd
kangyein9892 <inwon9892@naver.com>
2025-01-23 22:38:41 +0900

    delete/#93: Tour 화면 흐름 새롭게 설계

--- app/src/main/java/com/wearerommies/roomie/presentation/ui/tour/TourSideEffect.kt
+++
@@ -1,8 +0,0 @@
-package com.wearerommies.roomie.presentation.ui.tour
-
-sealed class TourSideEffect {
-    data object NavigateUp: TourSideEffect()
-    data object NavigateToSecondStep: TourSideEffect()
-    data object NavigateToThirdStep: TourSideEffect()
-    data object NavigateToCompletedStep: TourSideEffect()
-}
