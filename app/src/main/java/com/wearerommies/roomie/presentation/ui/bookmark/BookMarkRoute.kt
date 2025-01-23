13b524b93bdbc8c7df5c7d26685855ddf3b78419
hyeumm <20221159@sungshin.ac.kr>
2025-01-24 04:22:59 +0900

    chore/#101: 애니메이션 효과 및 기본 padding 제거

--- app/src/main/java/com/wearerommies/roomie/presentation/ui/bookmark/BookMarkRoute.kt
+++ app/src/main/java/com/wearerommies/roomie/presentation/ui/bookmark/BookMarkRoute.kt
@@ -8,7 +8,9 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Icon
@@ -136,7 +138,8 @@ fun BookMarkScreen(
         modifier = modifier
             .fillMaxSize()
             .background(RoomieTheme.colors.grayScale1)
-            .padding(paddingValues),
+            .statusBarsPadding()
+            .navigationBarsPadding(),
     ) {
         when (state) {
             is EmptyUiState.Loading -> {
