1025ec6027e243fa7d1ed3acf183bb4b1b27c43a
kangyein9892 <inwon9892@naver.com>
2025-01-24 16:42:39 +0900

    fix/#113: 투어 신청 두번째 버튼 활성화 여부 로직 수정

--- app/src/main/java/com/wearerommies/roomie/presentation/ui/tour/second/TourSecondState.kt
+++ app/src/main/java/com/wearerommies/roomie/presentation/ui/tour/second/TourSecondState.kt
@@ -1,7 +1,6 @@
 package com.wearerommies.roomie.presentation.ui.tour.second
 
 import com.wearerommies.roomie.domain.entity.TourEntity
-import com.wearerommies.roomie.presentation.core.util.RegexConstants.PHONE_NUMBER_REGEX
 
 data class TourSecondState(
     val uiState: TourEntity = TourEntity(),
@@ -10,5 +9,5 @@ data class TourSecondState(
     val isShowBirthDateModal: Boolean = false,
     val isValidated: Boolean = true
 ) {
-    val isSecondEnabledButton = uiState.name.isNotEmpty() && uiState.birthDate.isNotEmpty() && uiState.gender.isNotEmpty() && isValidated
+    val isSecondEnabledButton = uiState.name.isNotEmpty() && uiState.birthDate.isNotEmpty() && uiState.gender.isNotEmpty() && isValidated && uiState.phoneNumber.isNotEmpty()
 }
